bb4ca9a3b3d3f8d2186b4105a3c94922eb4e4fe0
babatonha <tonhaview@gmail.com>
2020-05-15 18:03:02 +0200

    End of module 9

---
+++ DatingApp-SPA/src/app/_resolvers/member-detail.resolver.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { User } from '../_models/user';
+import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { catchError } from 'rxjs/operators';
+import { of, Observable } from 'rxjs';
+
+@Injectable()
+export class MemberDetailsResolver implements Resolve<User> {
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private alertify: AlertifyService
+  ) {}
+
+  resolve(route: ActivatedRouteSnapshot): Observable<User> {
+    // tslint:disable-next-line: no-string-literal
+    return this.userService.getUser(route.params['id']).pipe(
+      catchError(error => {
+        this.alertify.error('Problem retrieing data');
+        this.router.navigate(['/members']);
+        return of(null);
+      })
+    );
+  }
+}
---
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/user';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+import {NgxGalleryOptions} from '@kolkov/ngx-gallery';
+import {NgxGalleryImage} from '@kolkov/ngx-gallery';
+import {NgxGalleryAnimation} from '@kolkov/ngx-gallery';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css'],
+})
+export class MemberDetailComponent implements OnInit {
+  user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
+  constructor(
+    private userService: UserService,
+    private alertify: AlertifyService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.data.subscribe((data) => {
+      // tslint:disable-next-line: no-string-literal
+      this.user = data['user'];
+    });
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false,
+      },
+    ];
+    this.galleryImages = this.getImages(); // npm install @kolkov/ngx-gallery --save
+  }
+
+  getImages() {
+    const imageUrls = [];
+    for (const photo of this.user.photos) {
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url,
+        description: photo.description,
+      });
+    }
+    return imageUrls;
+  }
+
+  // the + used below is because we are getting a string id from the route and we want an int
+  // loadUser() {
+  //   // tslint:disable-next-line: no-string-literal
+  //   this.userService.getUser(+this.route.snapshot.params['id']).subscribe(
+  //     (user: User) => {
+  //       this.user = user;
+  //     },
+  //     (error) => {
+  //       this.alertify.error(error);
+  //     }
+  //   );
+  // }
+}
